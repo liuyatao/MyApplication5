828ef9772f28aaf1c547bb218b1e43169be7e891
liuyatao <739697044@qq.com>
2015-09-06 16:54:21 +0800

    9月6日

--- business/src/main/java/com/lyt/business/fragment/ResSettingFragment.java
+++ business/src/main/java/com/lyt/business/fragment/ResSettingFragment.java
@@ -1,13 +1,14 @@
 package com.lyt.business.fragment;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amap.api.maps2d.MapView;
 import com.lyt.business.R;
@@ -23,10 +25,13 @@ import com.lyt.business.adapter.ResSettingImageAdapter;
 import net.yazeed44.imagepicker.model.ImageEntry;
 import net.yazeed44.imagepicker.util.Picker;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.bmob.v3.datatype.BmobFile;
+import cn.bmob.v3.listener.UploadFileListener;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
@@ -52,8 +57,9 @@ public class ResSettingFragment extends Fragment implements View.OnClickListener
     RecyclerView resSettingImages;
     private Context context;
     private ArrayList<String> imagePathArrayList=new ArrayList<>();
+    private ProgressDialog progressDialog;
+    ResSettingImageAdapter adapter;
 
-    private OnFragmentInteractionListener mListener;
     /**
      * 信息是否为空
      */
@@ -62,6 +68,8 @@ public class ResSettingFragment extends Fragment implements View.OnClickListener
      * 是否为编辑为编辑状态
      */
     boolean isEdit = false;
+
+    private static  String TAG="RESSEETING";
 
 
     public static ResSettingFragment newInstance() {
@@ -80,6 +88,7 @@ public class ResSettingFragment extends Fragment implements View.OnClickListener
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
 
+
     }
 
     @Override
@@ -97,7 +106,7 @@ public class ResSettingFragment extends Fragment implements View.OnClickListener
 
         LinearLayoutManager layoutManager=new LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false);
         resSettingImages.setLayoutManager(layoutManager);
-        ResSettingImageAdapter adapter=new ResSettingImageAdapter(context,imagePathArrayList);
+        adapter=new ResSettingImageAdapter(context,imagePathArrayList);
         resSettingImages.setAdapter(adapter);
 
     }
@@ -125,7 +134,7 @@ public class ResSettingFragment extends Fragment implements View.OnClickListener
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+
     }
 
     @Override
@@ -147,19 +156,53 @@ public class ResSettingFragment extends Fragment implements View.OnClickListener
        new  Picker.Builder(context, new Picker.PickListener() {
            @Override
            public void onPickedSuccessfully(ArrayList<ImageEntry> arrayList) {
+               final BmobFile bmobFile=new BmobFile(new File(arrayList.get(0).path));
+               progressDialog=new ProgressDialog(context);
+               progressDialog.show();
+               bmobFile.upload(context, new UploadFileListener() {
+                   @Override
+                   public void onSuccess() {
+                       Log.i(TAG, bmobFile.getFileUrl(context));
+                       bmobFile.loadImageThumbnail(context, resSettingHeadimage, 300, 300, 100);
+                       progressDialog.dismiss();
+                       Toast.makeText(context,"文件上传成功",Toast.LENGTH_LONG).show();
+                   }
 
+                   @Override
+                   public void onFailure(int i, String s) {
+
+                   }
+
+                   @Override
+                   public void onProgress(Integer value) {
+                       super.onProgress(value);
+                       Log.e(TAG, value + "");
+                   }
+               });
+
+              /* BmobProFile.getInstance(context).upload(arrayList.get(0).path, new UploadListener() {
+                   @Override
+                   public void onSuccess(String s, String s1, BmobFile bmobFile) {
+                       Log.e(TAG, "onSuccess");
+                   }
+
+                   @Override
+                   public void onProgress(int i) {
+                       Log.e(TAG,"onProgress");
+                   }
+
+                   @Override
+                   public void onError(int i, String s) {
+                       Log.e(TAG, "onError");
+                   }
+               });*/
            }
 
            @Override
            public void onCancel() {
 
            }
-       },R.style.pic).setPickMode(Picker.PickMode.SINGLE_IMAGE).setLimit(1).build().startActivity();
-    }
-
-
-    public interface OnFragmentInteractionListener {
-        public void onFragmentInteraction(Uri uri);
+       }, R.style.pic).setPickMode(Picker.PickMode.SINGLE_IMAGE).setLimit(1).build().startActivity();
     }
 
     @Override
